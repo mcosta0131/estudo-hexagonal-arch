c5543c808ee75db284ae3d744ae28f025ebde7f5
MATEUS COSTA DOS SANTOS <34768777+mcosta0131@users.noreply.github.com>
2025-04-20 16:44:09 -0300

    config: Configuração de qualifiers

--- src/main/java/com/estudohexagonalarch/application/service/DuplicataService.java
+++ src/main/java/com/estudohexagonalarch/application/service/DuplicataService.java
@@ -3,15 +3,12 @@ package com.estudohexagonalarch.application.service;
 import com.estudohexagonalarch.application.inbound.DuplicataUseCase;
 import com.estudohexagonalarch.application.outbound.DuplicataOutputPort;
 import com.estudohexagonalarch.domain.DuplicataDomain;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
 
-@Service
 public class DuplicataService implements DuplicataUseCase {
 
     private final DuplicataOutputPort duplicataOutPutPort;
 
-    public DuplicataService(@Qualifier("client") DuplicataOutputPort duplicataOutPutPort) {
+    public DuplicataService(DuplicataOutputPort duplicataOutPutPort) {
         this.duplicataOutPutPort = duplicataOutPutPort;
     }
 
---
+++ src/main/java/com/estudohexagonalarch/infrastructure/config/ApplicationConfig.java
@@ -0,0 +1,18 @@
+package com.estudohexagonalarch.infrastructure.config;
+
+import com.estudohexagonalarch.application.inbound.DuplicataUseCase;
+import com.estudohexagonalarch.application.service.DuplicataService;
+import com.estudohexagonalarch.application.outbound.DuplicataOutputPort;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public DuplicataUseCase duplicataUseCase(@Qualifier("client") DuplicataOutputPort duplicataOutputPort) {
+        return new DuplicataService(duplicataOutputPort);
+    }
+}
+
